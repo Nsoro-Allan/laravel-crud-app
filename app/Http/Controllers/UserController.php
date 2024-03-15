39478c51f8f7988a963b9d7597a09dbdfb07c850
AllanCorp <135638821+allancorp23@users.noreply.github.com>
2024-03-15 12:07:46 +0200

    login functionality added...

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,13 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class UserController extends Controller
 {
     public function register(Request $request){
         $incomingFields = $request->validate([
-            'email' => ['required', 'email'],
-            'name' => 'required',
+            'email' => ['required', 'email', Rule::unique('users', 'email')],
+            'name' => ['required', Rule::unique('users', 'name')],
             'password' => 'required'
         ]);
 
