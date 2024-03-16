153df1cfd56918292bb625a06e0bb4cad089293d
AllanCorp <135638821+allancorp23@users.noreply.github.com>
2024-03-16 11:06:54 +0200

    done...

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,6 +7,73 @@
     <title>Dashboard</title>
 </head>
 <body>
-    <h1>Welcome User...</h1>
+    <style>
+        body{
+            font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
+        }
+        .main-container{
+            display: flex;
+            flex-direction:column;
+            gap: 30px;
+            width: 100%;
+        }
+        .container{
+            padding: 5px;
+            border: 3px solid black;
+            border-radius: 6px;
+        }
+        .container form{
+            display: flex;
+            flex-direction:column;
+            gap: 15px;
+        }
+        .container form input{
+            padding: 10px;
+            border: 1.5px solid black;
+            border-radius: 6px;
+        }
+        .container form button{
+            padding: 10px;
+            width: 20%;
+            background: black;
+            color: #fff;
+            border: none;
+            border-radius: 6px;
+            font-weight: bold;
+            margin-bottom: 5px;
+        }
+        .container form button:hover{
+            cursor: pointer;            background: rgba(0, 0, 0, 0.658);
+        }
+        ::placeholder{
+            color: black;
+        }
+        h1{
+            padding: 10px 15px;
+            border-style: dashed;
+            font-style: italic;
+            font-size: 1.8rem;
+            background: black;
+            color: #fff;
+            border-radius: 6px;
+            border: 2px solid #fff;
+        }
+    </style>
+<div class="main-container">
+    <h1>Laravel Crud App</h1>
+
+    <div class="container">
+        <h1>Dashboard</h1>
+
+        @auth
+        <h1>Welcome User...</h1>
+        @else
+            <h1>You are not logged in...</h1>
+            <h2>Please Loggin here?</h2><a href="./home">login</a>
+        @endauth
+
+    </div>
+
+</div>
 </body>
 </html>
