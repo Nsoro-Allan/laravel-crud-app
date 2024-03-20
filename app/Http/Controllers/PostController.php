d3fa8daca514b88b90fb2f12dc5f58f82b6ac6a3
AllanCorp <135638821+allancorp23@users.noreply.github.com>
2024-03-20 14:19:17 +0200

    changes made..

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,8 +7,28 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
+    public function updatePost(Post $post, Request $request){
+        if(auth()->user()->id !== $post['user_id']){
+            return redirect('/home');
+        }
+
+        $incomingFields = $request->validate([
+            'title' =>'required',
+            'body' =>'required'
+        ]);
+
+        $incomingFields['title'] = strip_tags($incomingFields['title']);
+
+        $incomingFields['body'] = strip_tags($incomingFields['body']);
+
+        $post->update($incomingFields);
+        return redirect('/dashboard');
+    }
 
     public function showEdit(Post $post){
+        if(auth()->user()->id !== $post['user_id']){
+            return redirect('/home');
+        }
         return view('edit-post', ['post' => $post]);
     }
 
